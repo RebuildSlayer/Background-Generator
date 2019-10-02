98885da7b452f5f2701effebc44a0004544e1a5e
RebuildSlayer <mohammadibrahim.ibrahim9@gmail.com>
2019-10-02 16:39:32 +0500

    Adding Files

---
+++ BackJS.js
@@ -0,0 +1,24 @@
+var input1=document.getElementById("input1");
+var input2=document.getElementById("input2");
+var body=document.querySelector("body");
+var h3=document.querySelector("h3");
+var i1,i2;
+
+function color1()
+{
+	body.style.background =	
+	"linear-gradient(to right,"
+	+input1.value
+	+","
+	+input2.value
+	+")";
+	h3.innerHTML=
+	"linear-gradient ( to right, "
+	+input1.value.toUpperCase()
+	+", "
+	+input2.value.toUpperCase()
+	+" ) ";
+}
+
+input2,addEventListener("input",color1);
+input1.addEventListener("input",color1);
